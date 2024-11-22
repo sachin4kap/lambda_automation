bb2e22fcb2e3ee53058cee1a5d30f8e159049444
global4ventures <divit4globalventures@gmail.com>
2024-11-22 13:42:44 +0000

    commented code

--- publish.sh
+++ publish.sh
@@ -20,38 +20,38 @@ ACCOUNT_ID=$(aws sts get-caller-identity \
     --query Account \
     --output text)
 
-API_GATEWAY_ID=$(aws apigateway get-rest-apis \
-    --query "items[?name=='$PROJECT_NAME'].id" \
-    --region $AWS_REGION \
-    --output text)
-echo apigateway id: $API_GATEWAY_ID
+# API_GATEWAY_ID=$(aws apigateway get-rest-apis \
+#     --query "items[?name=='$PROJECT_NAME'].id" \
+#     --region $AWS_REGION \
+#     --output text)
+# echo apigateway id: $API_GATEWAY_ID
 
-echo aws lambda delete-alias $1
-aws lambda delete-alias \
-    --function-name $PROJECT_NAME \
-    --name $1 \
-    --region $AWS_REGION \
-    2>/dev/null
+# echo aws lambda delete-alias $1
+# aws lambda delete-alias \
+#     --function-name $PROJECT_NAME \
+#     --name $1 \
+#     --region $AWS_REGION \
+#     2>/dev/null
 
-echo zip hello.js
-cd lambda/
-rm --force hello.zip
-zip hello.zip hello.js
+# echo zip hello.js
+# cd lambda/
+# rm --force hello.zip
+# zip hello.zip hello.js
 
-echo aws lambda update-function-code $PROJECT_NAME
-aws lambda update-function-code \
-    --function-name $PROJECT_NAME \
-    --zip-file fileb://hello.zip \
-    --region $AWS_REGION
+# echo aws lambda update-function-code $PROJECT_NAME
+# aws lambda update-function-code \
+#     --function-name $PROJECT_NAME \
+#     --zip-file fileb://hello.zip \
+#     --region $AWS_REGION
 
-echo aws lambda update-function-code $PROJECT_NAME
-VERSION=$(aws lambda publish-version \
-    --function-name $PROJECT_NAME \
-    --description $1 \
-    --region $AWS_REGION \
-    --query Version \
-    --output text)
-echo published version: $VERSION
+# echo aws lambda update-function-code $PROJECT_NAME
+# VERSION=$(aws lambda publish-version \
+#     --function-name $PROJECT_NAME \
+#     --description $1 \
+#     --region $AWS_REGION \
+#     --query Version \
+#     --output text)
+# echo published version: $VERSION
 
 echo aws lambda create-alias $1
 aws lambda create-alias \
