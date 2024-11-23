43edac58e9f5950548aaa39d304096adce4c2cba
global4ventures <divit4globalventures@gmail.com>
2024-11-23 04:34:54 +0000

    update code

--- publish.sh
+++ publish.sh
@@ -1,4 +1,29 @@
-cd "$dir/infra"
-terraform init
-terraform plan
-terraform apply
+# cd "$dir/infra"
+# terraform init
+# terraform plan
+# terraform apply
+
+
+
+tf-init() {
+    cd "$dir/infra"
+    terraform init
+}
+
+tf-validate() {
+    cd "$dir/infra"
+    terraform fmt -recursive
+	terraform validate
+}
+
+tf-apply() {
+    cd "$dir/infra"
+    terraform plan -out=terraform.plan
+    terraform apply -auto-approve terraform.plan
+}
+
+tf-destroy() {
+    cd "$dir/infra"
+    terraform destroy \
+        -auto-approve
+}
