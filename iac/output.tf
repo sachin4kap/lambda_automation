60275d2ff7a706ff541b9576c80ebd00c744aa34
global4ventures <divit4globalventures@gmail.com>
2024-11-24 07:25:38 +0000

    delete output file

--- iac/output.tf
+++
@@ -1,9 +0,0 @@
-output "s3_bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = module.s3_bucket.bucket_name
-}
-
-output "bucket_policy" {
-  description = "The bucket policy JSON"
-  value       = aws_s3_bucket_policy.bucket_policy.policy
-}
