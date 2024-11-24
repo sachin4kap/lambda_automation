053842401d63b6a0b011466cccca4bc8243392d6
global4ventures <divit4globalventures@gmail.com>
2024-11-24 07:23:25 +0000

    update code

--- iac/main.tf
+++ iac/main.tf
@@ -1,11 +1,3 @@
 provider "aws" {
   region  = var.region
     }
-module "s3_bucket" {
-  source = "./s3"
-}
-
-module "s3_policy" {
-  source = "./s3_policy"
-  bucket_name = module.s3_bucket.bucket_name
-}
